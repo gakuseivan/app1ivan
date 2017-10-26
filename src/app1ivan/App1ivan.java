7f56e1fd7abbaf4362fbd4f8e974800dcb523eae
gakuseivan <gakuseivan@gmail.com>
2017-10-25 21:16:56 -0300

    Inicio Proyecto

---
+++ src/app1ivan/App1ivan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1ivan;
+
+/**
+ *
+ * @author sala306
+ */
+public class App1ivan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        float num1 = 60;
+        float num2 = 30;
+        
+        System.out.println("Suma: "+(num1+num2));
+        System.out.println("Resta: "+(num1-num2));
+        System.out.println("Multiplicacion: "+(num1*num2));
+        System.out.println("Division: "+(num1/num2));
+        System.out.println("Residuo: "+(num1%num2));
+        
+        // TODO code application logic here
+    }
+    
+}
